d1c75495dd014f266f736f0d8bf89697f0580e36
Ananth Rajaraman <Ananth.Rajaraman@ca.com>
2017-03-14 22:11:07 -0700

    Adding Adv filter, changing to 2.1, Ability to pick state

--- src/javascript/_ts_override_card_age_plugin.js
+++ src/javascript/_ts_override_card_age_plugin.js
@@ -36,3 +36,63 @@ Ext.override(Rally.ui.cardboard.plugin.CardAge, {
         });
     }
 });
+
+Ext.override(Rally.ui.cardboard.CardBoard, {
+    _createRow: function(rowConfig, applySort) {
+        var collapsed = false;
+        if (this.rowConfig && this.rowConfig.field && this.state && this.state.collapsedRows) {
+            var rowKey = this._getRowKey(this.rowConfig.field, rowConfig.value);
+            collapsed = this.state.collapsedRows.hasOwnProperty(rowKey);
+        }
+
+        var defaultRowConfig = {
+            el: this.getEl().down('tbody.columns'),
+            columns: this.columnDefinitions,
+            context: this.getAppContextOrEnvironmentContext(),
+            fieldDef: this.rowConfig && this.rowConfig.fieldDef,
+            collapsed: collapsed
+        };
+
+        if (this.rowConfig) {
+            if(this.rowConfig.headerConfig) {
+                defaultRowConfig.headerConfig = this.rowConfig.headerConfig;
+            }
+            if(this.rowConfig.sortField) {
+                defaultRowConfig.sortField = this.rowConfig.sortField;
+            }
+        }
+
+        var row = Ext.create('Rally.ui.cardboard.row.Row',
+            Ext.apply(defaultRowConfig, rowConfig)),
+            sortIndex = applySort ? this._getSortedIndex(row) : this.rowDefinitions.length;
+        this.rowDefinitions.splice(sortIndex, 0, row);
+        row.insert(this.rowDefinitions[sortIndex + 1]);
+
+        if(row.isCollapsible()) {
+            row.on('collapse', this.saveState, this);
+            row.on('expand', this.saveState, this);
+        }
+        return row;
+    },
+        _onColumnReady: function (column, records) {
+            this._columnsLoaded = this._columnsLoaded || 0;
+            this._columnsLoaded++;
+            this.fireEvent('columnready', column, this);
+            if (this._columnsLoaded === this.columnDefinitions.length) {
+                this._onAllColumnsReady();
+
+                if(!this.getRows().length) {
+                    if(this.rowConfig) {
+                        this._createRow({
+                            showHeader: true,
+                            value: null,
+                            isBlankSlate: true
+                        });
+                    } else {
+                        this._createDefaultRow();
+                    }
+                }
+            }
+        }
+
+});
